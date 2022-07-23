02b7ed232c9dd96de58d55d5d803611405f2bfc7
ShahidNarwade <94898655+ShahidNarwade@users.noreply.github.com>
2022-07-23 17:08:20 +0530

    Add files via upload
    21/7/2022

---
+++ AdditionOfdigit.java
@@ -0,0 +1,13 @@
+public class AdditionOfdigit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a= 89 ;
+		int b =90 ;
+		int add = a+ b ;
+		System.out.println("The Addition Of a and b is "+add);
+
+	}
+
+}
---
+++ Colleag.java
@@ -0,0 +1,31 @@
+public class Colleag {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String StudentName ="Shahid";
+		String Address ="Miraj ";
+		int Phonenumber =1234567891;
+		String Gender ="Male" ;
+		String DateOFBirth ="09/05/2001";
+		double height =6.6 ;
+		short Weight =63  ;
+		String Degree ="B.Tech";
+		int id =12345678 ;
+		
+		
+		System.out.println("The ID Of Student IS "+id);
+		System.out.println("The Student Name Is "+StudentName);
+		System.out.println("The Address Of Student "+Address);
+		System.out.println("The Phone Number Of Student Is "+Phonenumber);
+		System.out.println("Gender "+Gender);
+		System.out.println("The Date Of Birth Of Student Is "+DateOFBirth);
+		System.out.println("Height Of Student Is "+height);
+		System.out.println("The Weight Of Student Is "+Weight);
+		System.out.println("The Degree Of Student Is "+Degree);
+		
+		
+
+	}
+
+}
